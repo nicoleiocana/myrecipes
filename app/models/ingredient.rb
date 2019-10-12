fa332a356ca9fac62fdf9ac42436d771a7545ee3
Nicolei Ocana <nicoleiocana@gmail.com>
2019-10-12 14:50:46 +0000

    Complete ingredients resource

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,8 +1,15 @@
 class Ingredient < ApplicationRecord
+  before_save :name_capitalize
   
   validates :name, presence: true, length: { minimum: 3, maximum: 25 },
                                uniqueness: true
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
+  
+  private
+  
+    def name_capitalize
+      self.name = name.capitalize
+    end
   
 end
